59e9c629e63d18ca971072abda57e717e41523ad
Hevn <handikahevn24@gmail.com>
2018-12-08 10:32:24 +0700

    Base Project Api Larave 5.7

---
+++ app/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * This Story Exactly Same
+     * Will It Be The Same At The End ...??
+     * This is Like Infinity Nightmare or 
+     * Infinity Curse In My Live
+     */
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
